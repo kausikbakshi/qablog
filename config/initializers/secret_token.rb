ebdc2f86b22e76524af8c1e445d76c82c2351f8c
Kausik Bakshi <kausikgm@gmail.com>
2012-02-27 01:25:52 +0530

    structure commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qablog::Application.config.secret_token = 'ded4f7a13590750bc833b2709853ea3deb967b0acfbe1853bacc6f007f8e6a4c23786bef3a0c212df97bb05a87637da10926ea44715cbc1ebabcbb5c96c056ae'
