f0a0c83d8053fde6de9d58d7fdd12e84fcb3a439
Kausik Bakshi <kausikgm@gmail.com>
2012-02-29 01:41:23 +0530

    create function add question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,13 @@
 class AnswersController < ApplicationController
+
+  def create
+    @question = Question.find(params[:id])
+    @answer = @question.answers.build(params[:answer])
+    if @answer.save
+      redirect_to question_path(:id => @question.id), :notice => "Thank You For Posting Your Answer"
+    else
+      redirect_to question_path(:id => @question.id), :notice => "Your Answer Is Not Submit Due To Some Error"
+    end
+  end
+  
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,5 +17,6 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    @answers = @question.answers
   end
 end
